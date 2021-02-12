ccb77bf5825015b27457b463dd8d04c7eec5bde7
Eugina Palen <epalen@ucsd.edu>
2021-02-11 17:09:43 -0800

    AJAX lab done

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,4 +27,35 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+	//AJAX endpoint
+	$.get("/project/" + idNumber, callBackFn);
 }
+
+function callBackFn(result) {
+	console.log(result);
+	console.log("slowly....");
+	console.log(result['id']);
+	console.log('#' + result['id']);
+	var idNum = result['id'];
+	$('#project' + idNum).find('.thumbnail').find('.details').html(
+		/*HTML START*/
+	  	'<img src="' + result['image'] + '" class="detailsImage"></a>' +
+	  	'<h3>' + result['title'] + '</h3>' +
+		'<h4>' + result['date'] + '</h4>' +
+		'<p>' + result['summary'] + '</p>'
+		); /*HTML END*/
+/* 
+	var projectID = result['id'];
+	console.log(projectID);
+	$('.details', result['id']).html(<p>what goes on lmao</p>); */
+	
+}
+
+/*idk what this is*/
+/* function addProject(result) {
+	var projectHTML = '<a href="#" class="thumbnail">' +
+	  '<img src="' + result['image'] + '" class="img">' +
+	  '<p>' + result['title'] + '</p>' +
+	  '<p><small>' + result['date'] +
+	  '</small></p></a>';
+  } */
